435f518d750a05f85ff6362bdf5734dedeb92100
Kenrick <kenrick95@gmail.com>
2024-12-02 21:35:25 +0800

    feat: update

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import Slide from './Slides/Slide';
 import s from './App.module.css';
 import './Demo/TimetableTimes.scss';
 import { DemoTimetable01 } from './Demo/DemoTimetable01';
+import clsx from 'clsx';
+import excelSheetScreenshot from '/images/excel-itinerary.png';
 
 /*
 
@@ -29,15 +31,70 @@ function App() {
   return (
     <div className={s.root}>
       <Slide>
-        <h1>(Ab)using CSS Grid to Build Calendar View on the Web</h1>
+        <h1 className={s.textCenter}>
+          (Ab)using CSS Grid to Build Timetable View on the Web
+        </h1>
       </Slide>
       <Slide>
-        <h1>What</h1>
+        <h1>About Me</h1>
+        <img
+          src="https://1.gravatar.com/avatar/4e552084ecff437b1446c6c7182caff443b190d657f57f3c0eedb2ac413018f3?size=256"
+          width={128}
+          height={128}
+          alt="me"
+          className={clsx(s.avatar, s.asideRight)}
+        />
         <ul>
-          <li>Building itinerary planning web app</li>
-          <li>Idea: Specify date & time into calendar view</li>
-          <li>Calendar: columns are days, rows are time of day (24 hour)</li>
+          <li>Kenrick</li>
+          <li>Front-End Dev at Shopee</li>
+          <li>
+            Last time talked in{' '}
+            <a href="https://kenrick95.org/slides/nikku/">Geekcamp 2019</a>
+          </li>
         </ul>
+      </Slide>
+      <Slide>
+        <h1>Why</h1>
+        <span>Post-pandemic travelling</span>
+        {/* After pandemic, I've increased my travelling frequency. The most difficult part of travelling for me is the planning part. On what to do in the destinations. Because I wanted to have to make full use of my time. */}
+      </Slide>
+      <Slide>
+        <h1>Travel Planning</h1>
+        <ul>
+          <li>Excel sheet</li>
+          {/* With that, I usually have Excel sheets of the places I wanted to visit and plan the timings and routes on how go. */}
+          <li>Collaboration</li>
+          {/* Aside from that, I also travel together with friends, so have to take account on their preferences and schedules. */}
+        </ul>
+      </Slide>
+      <Slide>
+        {/* On my Excel sheet, I like to build itinerary as a timetable view. Because I can visualize my whole trip activity in one view. */}
+        {/* In this timetable view, the rows are each hour, the columns are days, the cells are the activity. Each activity can occupy more than 1 cell. */}
+        {/* With this view, if say I need to move around an activity to different day, it is very visible. */}
+        {/* Building it once is fine, however, when plan changes, it is quite a pain to edit them. Because now editing when there are changes is pain, because now I have to deal with cell unmerge and merge. */}
+        {/* Besides that, the granularity is per-hour, so I need to keep rounding up or down activity duration. */}
+        {/* Moreover, since I build this in timetable view, if I need a "day-by-day" plan, I need to manually copy over to a different sheet. */}
+        <img src={excelSheetScreenshot} className={s.imageFit} />
+        <span>
+          My itinerary Excel sheet for{' '}
+          <a href="https://blog.kenrick95.org/2024/05/japan-2024/">
+            my first Japan trip
+          </a>
+        </span>
+      </Slide>
+      <Slide>
+        <h1>Let's Build It Myself</h1>
+        <ul>
+          <li>Idea: Itinerary planning web app</li>
+          {/* Because of that, I thought, hey why not make this myself... */}
+          <li>Proof-of-concept: Timetable view</li>
+          {/* Of course, I tried some of the popular itinerary planning apps out there, but to my knowledge, none focuses on this timetable view as the center piece. */}
+          <li>Columns are days, rows are time of day (24 hour, minute-level granularity)</li>
+          {/* Let's make it very granular, at "minute" level of granularity */}
+        </ul>
+      </Slide>
+      <Slide>
+        Isn't it Google Calendar "week view"?
       </Slide>
       <Slide>
         <h1>Short Explainer on CSS Grid</h1>
@@ -84,7 +141,12 @@ function App() {
         </ul>
       </Slide>
       <Slide>
-        <h1></h1>
+        <h1>Links</h1>
+        <a href="https://ikuyo.kenrick95.org">🔗 ikuyo.kenrick95.org</a>
+        <a href="https://github.com/kenrick95/ikuyo">
+          🐙 github.com/kenrick95/ikuyo
+        </a>
+        <a href="https://bsky.app/profile/kenrick95.org">🦋 @kenrick95.org</a>
       </Slide>
     </div>
   );
